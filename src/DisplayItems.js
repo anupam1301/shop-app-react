0b892267e363b75184dd55000850a1aea2836c85
Anupam Anand <anupam1301@yahoo.com>
2018-10-10 15:10:55 +0530

    fetching images

--- src/DisplayItems.js
+++ src/DisplayItems.js
@@ -8,12 +8,25 @@ class DisplayItems extends Component {
     var name=this.props.name;
     var desc=this.props.desc;
     var price=(this.props.price)/100;
+    var images=this.props.images;
+    var imid=this.props.imid;
+
+
+
+        var file = images.find(function (el) {
+
+           return imid === el.id;
+        });
+
+
 
         return (
             <div>
-                {name}<br />
+                <img src={file.link.href} style={{width: '150px', height: '200px'}} alt="image not found." /><br />
+                <h4>{name}</h4><br />
                 {desc}<br />
-                $ {price}<br />
+                <h5>$ {price}</h5><br />
+                <button>Add To Cart</button>
             </div>
         );
     }
--- src/Main.js
+++ src/Main.js
@@ -12,23 +12,24 @@ class Main extends Component {
     constructor(props){
         super(props);
         this.state={
-            products:[]
+            products:[],
+            images:[]
 
         }
     }
 
     render() {
         const that= this;
-        var arr=[];
-        const products = Moltin.Products.All().then((products) => {
+
+        Moltin.Products.With('main_image').All().then((products) => {
             debugger;
             console.log(products);
-            that.setState({ products: products.data});
+            that.setState({ products: products.data, images:products.included.main_images});
         });
 
         var dispData = this.state.products.map((dat, i) => {
             debugger;
-            return <DisplayItems name={dat.name} price={dat.price[0].amount} desc={dat.description} uid={dat.id} key={i} ki={i}/> ;
+            return <DisplayItems name={dat.name} images={this.state.images} imid={dat.relationships.main_image.data.id} price={dat.price[0].amount} desc={dat.description} uid={dat.id} key={i} ki={i}/> ;
 
         });
 
